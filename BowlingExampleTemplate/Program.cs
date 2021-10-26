15db1683f374f4c251693d4722c2ba3a6c3749d1
DESKTOP-FIB550G\Kile <nimeria83@yahoo.com>
2021-10-26 13:05:07 +0200

    Bowling score game

--- BowlingExampleTemplate/Program.cs
+++ BowlingExampleTemplate/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BowlingExampleTemplate.Bowling;
+using System;
 
 namespace BowlingExampleTemplate
 {
@@ -6,7 +7,33 @@ namespace BowlingExampleTemplate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test Commit");
+            BowlingLogic bowlingLogic = new BowlingLogic();
+            BowlingLogic bowlingLogic2 = new BowlingLogic();
+
+            /*rollMany(12, 10, bowlingLogic);
+            int result = bowlingLogic.Score();
+            Console.WriteLine(result);*/
+
+            rollSpare(bowlingLogic2);
+            int result2 = bowlingLogic2.Score();
+            Console.WriteLine("Roll spare is: " + result2);
+           
+        }
+        private static void rollMany(int rolls, int pins, BowlingLogic bowlingLogic)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                bowlingLogic.Roll(pins);
+            }
+        }
+
+        private static void rollSpare(BowlingLogic bowlingLogic2)
+        {
+            //bowlingLogic2.Roll(new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 0 });
+            bowlingLogic2.Roll(5);
+            bowlingLogic2.Roll(5);
+            bowlingLogic2.Roll(5);
+            bowlingLogic2.Roll(3);
         }
     }
 }
